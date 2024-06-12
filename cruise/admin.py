03d81babf8aa50c5e008b69ccc717faf546d4fe2
Helena Björnberg <helena.bjornberg@hotmail.com>
2024-06-12 20:00:03 +0000

    Add Cruise model

--- cruise/admin.py
+++ cruise/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cruise
 
 # Register your models here.
+admin.site.register(Cruise)
--- cruise/models.py
+++ cruise/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Cruise(models.Model): 
+    cruise_number = models.CharField(max_length=15, unique=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self): 
+        return f"Cruise {self.cruise_number} from {self.start_date} to {self.end_date}"
