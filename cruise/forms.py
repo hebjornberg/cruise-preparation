295a690dfb7083deae77ebfbc53b0e4d6580eb8a
Helena Björnberg <helena.bjornberg@hotmail.com>
2024-06-18 22:01:50 +0000

    Add format for input field

--- cruise/forms.py
+++ cruise/forms.py
@@ -5,6 +5,10 @@ class CruiseForm(forms.ModelForm):
     class Meta: 
         model = Cruise
         fields = ['cruise_number', 'start_date', 'end_date']
+        widgets = {
+            'start_date': forms.DateInput(attrs={'placeholder': 'Format: YYYY-MM-DD'}),
+            'end_date': forms.DateInput(attrs={'placeholder': 'Format: YYYY-MM-DD'}),
+        }
 
 class ItemForm(forms.ModelForm):
     class Meta: 
